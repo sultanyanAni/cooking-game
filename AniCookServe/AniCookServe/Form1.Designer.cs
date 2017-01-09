d83705858e83e102f88505f9817fbdb7dab4b277
Ani Sultanyan <sultanyanani@gmail.com>
2017-01-09 11:42:27 -0800

    added a dictionary of ingredients

---
+++ AniCookServe/AniCookServe/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace AniCookServe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.testLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.letterLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // testLabel
+            // 
+            this.testLabel.AutoSize = true;
+            this.testLabel.Location = new System.Drawing.Point(107, 111);
+            this.testLabel.Name = "testLabel";
+            this.testLabel.Size = new System.Drawing.Size(35, 13);
+            this.testLabel.TabIndex = 0;
+            this.testLabel.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(86, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(85, 61);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // letterLabel
+            // 
+            this.letterLabel.AutoSize = true;
+            this.letterLabel.Location = new System.Drawing.Point(66, 111);
+            this.letterLabel.Name = "letterLabel";
+            this.letterLabel.Size = new System.Drawing.Size(35, 13);
+            this.letterLabel.TabIndex = 2;
+            this.letterLabel.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.letterLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.testLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label testLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label letterLabel;
+    }
+}
+
---
+++ AniCookServe/AniCookServe/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AniCookServe
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, string> ingredients = new Dictionary<string, string>();
+        Random gen = new Random();
+        void init()
+        {
+     
+            //creating a dictionary of ingrediencts so they can be stored with a corresponding letter index
+            
+            ingredients.Add("C", "Cheese");
+            ingredients.Add("M", "Marinara");
+            ingredients.Add("A", "Alfredo");
+            ingredients.Add("P", "Pepperoni");
+            ingredients.Add("K", "Chicken");
+            ingredients.Add("J", "Jalapenos");
+            ingredients.Add("S", "Mushrooms");
+            ingredients.Add("O", "Olives");
+            ingredients.Add("B","Bacon");
+            ingredients.Add("Q", "Barbeque")
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            init();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string test = ingredients.Keys.ElementAt(gen.Next(1, 8));
+            testLabel.Text = ingredients[test];
+            letterLabel.Text = test; 
+        }
+    }
+}
